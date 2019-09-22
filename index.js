3de5a427127c14429c2b15572b661ea9e6d23011
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-09-21 21:13:55 -0700

    create temp test file for sql

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const oracledb = require("oracledb");
-const dbconfig = require("../dbconfig.js");
 const fs = require('fs'); 
 const csv = require('csv-parser');
+const readline = require('readline');
 
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 const dataFile = "../Loan.csv";
-fs.createReadStream(dataFile);
+let count = 0;
 
-async function connectToDB() {
-    let connection;
-    try {
-        connection = await oracledb.getConnection(dbconfig);
-        console.log("connected.");
-        let result = connection.execute(`SELECT * FROM LOAN;`);
-        console.log(result.rows);
-    } catch (err) {
-        console.log("Not connected: " + err);
-    } finally {
-        if (connection) {
-            try {
-                connection.close();
-            } catch (err) {
-                console.error("Connection Error: " + err);
-            }
-        }
-    }
-  }
-//connectToDB();
+var read = readline.createInterface({
+  input: fs.createReadStream(dataFile),
+  output: process.stdout,
+  console: false
+});
+
+readFile();
+function readFile(){
+
+  let sqlTableString = "DROP TABLE Loan;\n\n CREATE TABLE Loan(\nAmount Number(10) NOT NULL,\n Loan_Date Date, \n Loan_Title VARCHAR2(1000) NOT NULL,\n Risk_Score NUMBER(3) NOT NULL, \n Debt_To_Income NUMBER(3) NOT NULL,\n Zipcode NUMBER(5) NOT NULL,\n State VARCHAR2(2) NOT NULL,\nEmployment NUMBER(1) NOT NULL,\n Policy_Code NUMBER(1)\n);\n";
+
+  fs.writeFile("temp.sql", sqlTableString, (err)=>{
+    if(err)
+      console.log("error writing file: "+err);
+  });
+
+  // read.on("line", function(line){
+  //   count++;
+  //   if(count > 1){
+  //     console.log(line);
+  //   }
+  // });
+}
