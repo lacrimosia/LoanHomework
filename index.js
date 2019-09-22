4caabf61d8f5b7f18c622cbe00ac778813812adc
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-09-21 23:02:22 -0700

    removed punctuation and added insert statements into file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const readline = require('readline');
-
 const dataFile = "../Loan.csv";
 const sqlFile = "temp.sql";
+const tableName = "Loan";
+const removePunctuation = require('remove-punctuation');
 let count = 0;
 
 var read = readline.createInterface({
@@ -14,7 +15,7 @@ var read = readline.createInterface({
 readFile();
 
 function readFile() {
-    let sqlTableString = "DROP TABLE Loan;\n\n CREATE TABLE Loan(\nAmount VARCHAR2(10) NOT NULL,\n Loan_Date VARCHAR2(10), \n Loan_Title VARCHAR2(1000) NOT NULL,\n Risk_Score VARCHAR2(3) NOT NULL, \n Debt_To_Income VARCHAR2(3) NOT NULL,\n Zipcode VARCHAR2(5) NOT NULL,\n State VARCHAR2(2) NOT NULL,\nEmployment VARCHAR2(1) NOT NULL,\n Policy_Code VARCHAR2(1)\n);\n\n";
+    let sqlTableString = "DROP TABLE " + tableName + ";\n\n CREATE TABLE Loan(\nAmount VARCHAR2(100) NOT NULL,\n Loan_Date VARCHAR2(100), \n Loan_Title VARCHAR2(1000) NOT NULL,\n Risk_Score VARCHAR2(30) NOT NULL, \n Debt_To_Income VARCHAR2(30) NOT NULL,\n Zipcode VARCHAR2(5) NOT NULL,\n State VARCHAR2(2) NOT NULL,\nEmployment VARCHAR2(1) NOT NULL,\n Policy_Code VARCHAR2(1)\n);\n\n";
 
     fs.writeFile(sqlFile, sqlTableString, (err) => {
         if (err)
@@ -25,18 +26,24 @@ function readFile() {
         count++;
         let data, intoString = '';
         if (count > 1) {
-            
+
             data = line.split(','); //array
-            for(let i=0; i<data.length; i++){
-              intoString += "'"+data[i]+"', ";
+            for (let i = 0; i < data.length; i++) {
+                var length = data.length;
+                if (i < length - 1)
+                    intoString += "'" + removePunctuation(data[i]) + "', ";
+                else
+                    intoString += "'" + removePunctuation(data[i]) + "'";
             }
-            console.log(intoString+"\n");
-            data2 = "INSERT INTO Loan VALUES("+intoString+");\n\n";
-            fs.appendFile(sqlFile, data2, (err) => {
+            //console.log(intoString + "\n");
+            //let endingStatment = "\n select count(*) from Loan;";
+            data2 = "INSERT INTO " + tableName + " VALUES(" + intoString + ");\n\n";
+            let tableData = data2;
+            fs.appendFile(sqlFile, tableData, (err) => {
                 if (err)
                     console.log("error appending file: " + err);
             });
-            
         }
     });
+
 }
